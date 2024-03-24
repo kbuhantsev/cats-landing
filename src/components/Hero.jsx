b2e07455011b542da4a48fe77c074eca91e7ff77
kbuhantsev <k.buhantsev@gmail.com>
2024-03-24 20:08:30 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,6 @@
 const Hero = () => {
   return (
     <section className="relative">
-      {/* <img src="public/images/hero/hero_width.jpg" alt="sdcasd" /> */}
-      {/* <img
-        srcSet="public/images/hero/hero_mobile.jpg 640w, public/images/hero/hero_width.jpg 1536w"
-        sizes="(max-width: 640px) 640px, 1536px"
-        src="public/images/hero/hero_width.jpg"
-        alt="Інтерактивна кішка"
-      /> */}
-
       <picture>
         <source
           media="(max-width: 640px)"
