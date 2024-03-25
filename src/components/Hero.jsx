86d3802df254ce702c826b2234e0e5da38dfe449
kbuhantsev <k.buhantsev@gmail.com>
2024-03-25 13:56:45 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
       </picture>
 
       <h1
-        className="absolute left-5 top-4 -rotate-12 font-marckScript text-5xl font-bold text-accentRed
+        className="absolute left-0 top-0 w-full bg-accentPink text-center font-marckScript text-5xl font-bold text-white
       sm:relative sm:left-auto sm:top-auto sm:flex sm:rotate-0 sm:justify-center"
       >
         Інтерактивна кішка
