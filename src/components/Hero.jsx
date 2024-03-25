b78063715752393fe4ba8f8476a604110f95ddd1
kbuhantsev <k.buhantsev@gmail.com>
2024-03-25 13:21:21 +0200

    sdad

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,17 +4,17 @@ const Hero = () => {
       <picture className="mb-3 flex">
         <source
           media="(max-width: 640px)"
-          srcSet="/images/hero/hero_mobile.jpg"
+          srcSet="./images/hero/hero_mobile.jpg"
         />
         <source
           media="(min-width: 641px)"
-          srcSet="/images/hero/hero_width.jpg"
+          srcSet="./images/hero/hero_width.jpg"
         />
-        <img src="/images/hero/hero_mobile.jpg" alt="Інтерактивна кішка" />
+        <img src="./images/hero/hero_mobile.jpg" alt="Інтерактивна кішка" />
       </picture>
 
       <h1
-        className="text-accentRed font-marckScript absolute left-5 top-4 -rotate-12 text-5xl font-bold
+        className="absolute left-5 top-4 -rotate-12 font-marckScript text-5xl font-bold text-accentRed
       sm:relative sm:left-auto sm:top-auto sm:flex sm:rotate-0 sm:justify-center"
       >
         Інтерактивна кішка
